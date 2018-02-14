1988d88e91379059b3c5109557c71400ce3e34da
mbpsep17 10_13 <richard@samsc.com>
2018-02-14 14:05:05 +0000

    formatting

--- AVGreenScreenPlayer/GSApplicationDelegate.m
+++ AVGreenScreenPlayer/GSApplicationDelegate.m
@@ -57,7 +57,7 @@
 - (void)applicationDidBecomeActive:(NSNotification *)aNotification
 {
     NSArray *docs = [[NSDocumentController sharedDocumentController] documents];
-    if ([docs count]==0 && [[NSApplication sharedApplication] keyWindow] == nil)
+    if ( [docs count]==0 && [[NSApplication sharedApplication] keyWindow ] == nil)
     	{
         // If no docs already open, put up open file dialog at program launch
         [ [NSDocumentController sharedDocumentController] openDocument:self ];
